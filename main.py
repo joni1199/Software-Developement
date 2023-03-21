3611309e98a2153864c9fcbb33c91e4d6d9ac149
joni1199 <128416470+joni1199@users.noreply.github.com>
2023-03-21 11:15:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
 if __name__ == '__main__':
-    print('Software Developement')
-    print("Test")
-    print("Hello Jonathan!")
-    i = 5
-    if i<10:
-        print("i ist kleiner 10")
+    print('Startpunkt')
