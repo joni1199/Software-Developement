1786d13fee88b6148f45e5e6fb409f32b1512258
joni1199 <128416470+joni1199@users.noreply.github.com>
2023-03-31 18:22:27 +0200

    GUI

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry('150x220')
+window.title('Taschenechner')
+
+gui_items = list()
+button_values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+                 '+', '-', '*', '/', '=', 'AC']
+
+calculation = str()
+
+def create_button(value):
+    button = tk.Button(text=value)
+    gui_items.append(button)
+
+for val in button_values:
+    create_button(val)
+
+output_label = tk.Label(text='Hallo')
+output_label.grid(row=0, columnspan=10)
+
+column_count = 0
+row_count = 1
+max_columns = 3
+
+for item in gui_items:
+    item.grid(row=row_count, column=column_count)
+
+    column_count += 1
+    if column_count == max_columns:
+        column_count = 0
+        row_count += 1
+
 if __name__ == '__main__':
-    print('Startpunkt')
+    window.mainloop()
