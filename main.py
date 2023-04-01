a6e8ca1252695424a84d13bd7c24927a5092a6a3
joni1199 <128416470+joni1199@users.noreply.github.com>
2023-04-01 17:57:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tkinter as tk
 window = tk.Tk()
 window.geometry('150x220')
 window.title('Taschenechner')
+canvas = tk.Canvas(window, width=100, height=50, highlightthickness=0)
+canvas.pack()
 
 gui_items = list()
 button_values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
@@ -10,6 +12,32 @@ button_values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
 
 calculation = str()
 
+def create_rounded_rect(canvas, x1, y1, x2, y2, radius, **kwargs):
+    points = [x1 + radius, y1,
+              x1 + radius, y1,
+              x2 - radius, y1,
+              x2 - radius, y1,
+              x2, y1,
+              x2, y1 + radius,
+              x2, y1 + radius,
+              x2, y2 - radius,
+              x2, y2 - radius,
+              x2, y2,
+              x2 - radius, y2,
+              x2 - radius, y2,
+              x1 + radius, y2,
+              x1 + radius, y2,
+              x1, y2,
+              x1, y2 - radius,
+              x1, y2 - radius,
+              x1, y1 + radius,
+              x1, y1 + radius,
+              x1, y1]
+    return canvas.create_polygon(points, **kwargs, smooth=True, tags='button')
+
+text = canvas
+
+button = create_rounded_rect(canvas, 10, 10, 90, 40, 10, fill='blue')
 def create_button(value):
     button = tk.Button(text=value)
     gui_items.append(button)
